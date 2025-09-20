9f6761879c7af9adfb946ebe022fe77a0213e218
Zaloog <gramslars@gmail.com>
2025-09-20 17:47:12 +0200

    Add multi select opening to telescope

--- neovim/.config/nvim/lua/zaloog/lazy/telescope.lua
+++ neovim/.config/nvim/lua/zaloog/lazy/telescope.lua
@@ -1,3 +1,17 @@
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
@@ -16,6 +30,7 @@ return {
            i = {  -- Insert mode mappings
             ["<C-j>"] = require('telescope.actions').move_selection_next,
             ["<C-k>"] = require('telescope.actions').move_selection_previous,
+            ["<CR>"] = select_one_or_multi,
           },
         }
       }
